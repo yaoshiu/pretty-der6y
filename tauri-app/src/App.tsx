505bd8fe6466b7e8969ab8a63c34064fd36cc82b
Fay Ash <yaoshiu@qq.com>
2024-08-28 23:50:36 +0800

    feat: change update installation behavior to install on shutdown
    Modified the update process to download updates when detected, but delay the installation until the application is closed, improving user experience by avoiding interruptions.

--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -2,6 +2,7 @@ import {
   createContext,
   createSignal,
   lazy,
+  onCleanup,
   onMount,
   Show,
   useContext,
@@ -9,7 +10,7 @@ import {
 import Login from "./Login.tsx";
 import { LoggerProvider, useLogger } from "./components/Logger.tsx";
 import TittleBar from "./components/TittleBar.tsx";
-import { check } from "@tauri-apps/plugin-updater";
+import { check, type Update } from "@tauri-apps/plugin-updater";
 
 const Main = lazy(() => import("./Main.tsx"));
 
@@ -22,20 +23,29 @@ export function useLogined() {
 const Body = () => {
   const [logined] = useLogined();
   const logger = useLogger();
+  const [update, setUpdate] = createSignal<Update | null>(null);
 
   onMount(async () => {
     try {
-      const update = await check();
+      setUpdate(await check());
       logger?.info(
-        update?.available
-          ? `${update?.version} is available! Downloading...`
+        update()?.available
+          ? `${update()?.version} is available! Downloading...`
           : `Newest release!`
       );
-      await update?.downloadAndInstall((event) => {
+      await update()?.download((event) => {
         if (event.event === "Finished") {
           logger?.info("Update downloaded, restart to apply!");
         }
       });
+    } catch (error: any) {
+      logger?.error(error.toString());
+    }
+  });
+
+  onCleanup(async () => {
+    try {
+      await update()?.install();
     } catch (error: any) {
       logger?.error(error.toString());
     }
