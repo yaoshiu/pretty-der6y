d65ad2d847cc0c9e4bfadb3632da7c25a0a63f42
Fay Ash <yaoshiu@qq.com>
2024-08-28 18:55:01 +0800

    feat: add updater

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,5 @@
 use std::error::Error;
 
-use chrono::Local;
-use lib::Account;
-
-const GEOJSON_STR: &str = include_str!("../assets/map.geojson");
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     todo!()
--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -1,14 +1,47 @@
-import { createContext, createSignal, Show, useContext } from "solid-js";
+import {
+  createContext,
+  createSignal,
+  lazy,
+  onMount,
+  Show,
+  useContext,
+} from "solid-js";
 import Login from "./Login.tsx";
-import { LoggerProvider } from "./components/Logger.tsx";
+import { LoggerProvider, useLogger } from "./components/Logger.tsx";
 import TittleBar from "./components/TittleBar.tsx";
-import Main from "./Main.tsx";
+import { check } from "@tauri-apps/plugin-updater";
+
+const Main = lazy(() => import("./Main.tsx"));
 
 const LoginedContext = createContext(createSignal(false));
 
 export function useLogined() {
   return useContext(LoginedContext);
 }
+
+const Body = () => {
+  const [logined] = useLogined();
+  const logger = useLogger();
+
+  onMount(async () => {
+    try {
+      const update = await check();
+      await update?.downloadAndInstall((event) => {
+        if (event.event === "Finished") {
+          logger?.info("Update downloaded, restart to apply!");
+        }
+      });
+    } catch (error: any) {
+      logger?.error(error.toString());
+    }
+  });
+
+  return (
+    <Show when={logined()} fallback={<Login />}>
+      <Main />
+    </Show>
+  );
+};
 
 const App = () => {
   const [logined, setLogined] = createSignal(false);
@@ -25,9 +58,7 @@ const App = () => {
       >
         <TittleBar />
         <LoginedContext.Provider value={[logined, setLogined]}>
-          <Show when={logined()} fallback={<Login />}>
-            <Main />
-          </Show>
+          <Body />
         </LoginedContext.Provider>
       </div>
     </LoggerProvider>
