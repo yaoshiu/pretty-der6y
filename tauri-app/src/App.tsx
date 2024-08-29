234c1fa5809efa277174e9f5a2a50cd6b2c29704
Fay Ash <yaoshiu@qq.com>
2024-08-30 04:22:15 +0800

    fix: the install when exiting don't function as I imagined, so just giving it up.

--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -1,11 +1,10 @@
 import {
-  createContext,
-  createSignal,
-  lazy,
-  onCleanup,
-  onMount,
-  Show,
-  useContext,
+	createContext,
+	createSignal,
+	lazy,
+	onMount,
+	Show,
+	useContext,
 } from "solid-js";
 import Login from "./Login.tsx";
 import { LoggerProvider, useLogger } from "./components/Logger.tsx";
@@ -17,71 +16,67 @@ const Main = lazy(() => import("./Main.tsx"));
 const LoginedContext = createContext(createSignal(false));
 
 export function useLogined() {
-  return useContext(LoginedContext);
+	return useContext(LoginedContext);
 }
 
 const Body = () => {
-  const [logined] = useLogined();
-  const logger = useLogger();
-  const [update, setUpdate] = createSignal<Update | null>(null);
+	const [logined] = useLogined();
+	const logger = useLogger();
+	const [update, setUpdate] = createSignal<Update | null>(null);
 
-  onMount(async () => {
-    try {
-      logger?.info("Checking for updates...");
-      setUpdate(await check());
-      logger?.info(
-        update()?.available
-          ? `${update()?.version} is available! Downloading...`
-          : "Newest release!"
-      );
-      await update()?.download((event) => {
-        if (event.event === "Finished") {
-          logger?.info("Update downloaded, restart to apply!");
-        }
-      });
-    } catch (error) {
-      logger?.error(error as string);
-    }
-  });
+	onMount(async () => {
+		try {
+			logger?.info("Checking for updates...");
+			setUpdate(await check());
+			logger?.info(
+				update()?.available
+					? `${update()?.version} is available! Downloading...`
+					: "Newest release!",
+			);
+			await update()?.downloadAndInstall((event) => {
+				if (event.event === "Finished") {
+					logger?.info("Update installed, restart to apply!");
+				}
+			});
+		} catch (error) {
+			if (typeof error === "string") {
+				logger?.error(error);
+			} else if (error instanceof Error) {
+				logger?.error(error.message);
+			}
+		}
+	});
 
-  onCleanup(async () => {
-    try {
-      await update()?.install();
-    } catch (error) {
-      logger?.error(error as string);
-    }
-  });
-
-  return (
-    <Show when={logined()} fallback={<Login />}>
-      <Main />
-    </Show>
-  );
+	return (
+		<Show when={logined()} fallback={<Login />}>
+			<Main />
+		</Show>
+	);
 };
 
 const App = () => {
-  const [logined, setLogined] = createSignal(false);
+	const [logined, setLogined] = createSignal(false);
 
-  return (
-    <LoggerProvider>
-      <div
-        onContextMenu={(event) => {
-          // tricky way to enable right click on dev server
-          if (window.location.hostname === "tauri.localhost") {
-            event.preventDefault();
-          }
-        }}
-        class="select-none"
-      >
-        <div class="hidden md:block">
-          <TittleBar />
-        </div>
-        <LoginedContext.Provider value={[logined, setLogined]}>
-          <Body />
-        </LoginedContext.Provider>
-      </div>
-    </LoggerProvider>
-  );
+	return (
+		<LoggerProvider>
+			<div
+				onContextMenu={(event) => {
+					// tricky way to enable right click on dev server
+					if (window.location.hostname === "tauri.localhost") {
+						event.preventDefault();
+					}
+				}}
+				class="select-none"
+			>
+				<div class="hidden md:block">
+					<TittleBar />
+				</div>
+				<LoginedContext.Provider value={[logined, setLogined]}>
+					<Body />
+				</LoginedContext.Provider>
+			</div>
+		</LoggerProvider>
+	);
 };
 
 export default App;
