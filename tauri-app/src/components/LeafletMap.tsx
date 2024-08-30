ffe4930ab8a9aadb9801f0dba546b209b6437914
Fay Ash <yaoshiu@qq.com>
2024-08-31 03:55:48 +0800

    feat: add logger prompt for `LeafletMap`

--- tauri-app/src/components/LeafletMap.tsx
+++ tauri-app/src/components/LeafletMap.tsx
@@ -1,6 +1,7 @@
 import * as L from "leaflet";
 import { type JSX, onMount, type Signal, splitProps } from "solid-js";
 import "leaflet/dist/leaflet.css";
+import { useLogger } from "./Logger";
 
 export const LeafletMap = (
   props: JSX.HTMLAttributes<HTMLDivElement> & {
@@ -9,20 +10,27 @@ export const LeafletMap = (
 ) => {
   const [local, others] = splitProps(props, ["ref", "map"]);
   const [, setMap] = local.map;
+  const logger = useLogger();
 
   let ref!: HTMLDivElement;
 
   onMount(() => {
     const map = L.map(ref);
     setMap(map);
-    navigator.geolocation.getCurrentPosition((position) => {
-      map.setView([position.coords.latitude, position.coords.longitude], 13);
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        map.setView([position.coords.latitude, position.coords.longitude], 13);
+      },
+      (error) => {
+        logger?.error(`Error getting location: ${error.message}`);
+        map.setView([51.505, -0.09], 13);
+      },
+    );
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
   });
 
   return (
